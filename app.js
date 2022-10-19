9aadd27fe022d2daadb50be3190d8ab8e4fec60d
Lecislav <lecislav@protonmail.com>
2022-10-19 20:54:02 +0200

    add response object

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http  = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method,req.headers);
+
+    res.setHeader('Contet-Type','text/html');
+    res.write('<html>');
+    res.write('<head><tittle>My firts page</tittle></head>');
+    res.write('</head>');
+    res.end();
 });
 
 server.listen(3000);
