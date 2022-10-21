d72b753b4631fda1e04b8abe7e8d78acb44c0a5d
Lecislav <lecislav@protonmail.com>
2022-10-22 01:14:09 +0200

    nic

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ const server = http.createServer((req,res)=>{
     const url=req.url;
     const method =req.method;
     if(url==='/'){
-        res.setHeader('Contet-Type','text/html');
         res.write('<html>');
         res.write('<head><tittle>Enter message</tittle></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</head>');
         res.write('</html>');
-        res.end();
         return res.end();
     }
     if(url=== '/message' && method ==='POST'){
@@ -21,15 +19,16 @@ const server = http.createServer((req,res)=>{
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end',()=>{
+       return req.on('end',()=>{
             const parsedBody = Buffer.concat(body).toString();
             const message =parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
+            fs.writeFile('message.txt',message,(err)=>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
         });
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
+
     }
     res.setHeader('Contet-Type','text/html');
     res.write('<html>');
